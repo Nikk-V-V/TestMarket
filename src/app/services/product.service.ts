6755b79f0a862cbfb166331837a5d1c9d3e93a88
Nikk_o_O <44206804+Nikk-V-V@users.noreply.github.com>
2021-05-26 01:09:27 +0300

    init project

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {Product} from '../interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  products: Product[] = [
+    {id: 1, name: 'article 1', label : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', price: 25},
+    {id: 2, name: 'article 2', label: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', price: 35},
+    {id : 3, name: 'article 3', label: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', price: 45}
+  ];
+
+  constructor() { }
+
+  getAll(): Product[] {
+    return this.products;
+  }
+
+  addToCart(id: number): void {
+
+  }
+
+  // getCartData(): Product[] {
+  //
+  // }
+
+  // delete(id: number): Product[] {
+  //
+  // }
+
+  clear(): void {
+    localStorage.clear();
+  }
+}
